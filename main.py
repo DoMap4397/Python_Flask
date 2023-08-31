d333f7215605c97462dc6ad9b310ac0b68c2fa06
Do <do.nguyen.4397@gmail.com>
2023-08-31 15:45:48 +0700

    add create

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ app = Flask(__name__)
 @app.route('/home')
 def main():
     data = list(collections.find({}))
-    return render_template('home.html',data=data)
+    return render_template('home.html', data=data)
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
 
 @app.route('/test_db')
 def test_db():
     data = list(collections.find({}))
     return render_template("test_db.html", data=data)
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run(debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
